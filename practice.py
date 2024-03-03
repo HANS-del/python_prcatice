056b4e9400fbc77439d6b944e9dc0ba191028efc
HANSU <hahns38@gmail.com>
2024-03-03 17:50:40 +0900

    study_24_03_03

--- practice.py
+++ practice.py
@@ -716,24 +716,130 @@ def add(a,b):
 # myfunc()
 
 
-import time
+# import time
 
-def elapsed(original_func):
-    def wrapper():
-        start = time.time()
-        result = original_func()
-        end = time.time()
-        print("함수 수행 시간 : %f초" % (end - start))
-        return result
-    return wrapper
+# def elapsed(original_func):
+#     def wrapper(*args, **kwargs):
+#         start = time.time()
+#         result = original_func(*args, **kwargs)
+#         end = time.time()
+#         print("함수 수행 시간 : %f초" % (end - start))
+#         return result
+#     return wrapper
 
 
 
-@elapsed
-def myfunc():
-    print("함수가 실행됩니다.")
+# @elapsed
+# def myfunc(msg):
+#     print("'%s' 을 출력합니다." % msg)
 
-# decorated_myfunc = elapsed(myfunc)
-# decorated_myfunc()
+# # decorated_myfunc = elapsed(myfunc)
+# # decorated_myfunc()
 
-myfunc()
+# myfunc("you need python")
+
+
+# class MyIterator :
+#     def __init__(self, data):
+#         self.data = data
+#         self.position = 0
+
+#     def __iter__(self):
+#         return self
+    
+#     def __next__(self):
+#         if self.position >= len(self.data):
+#             raise StopIteration
+#         result = self.data[self.position]
+#         self.position +=1
+#         return result
+    
+# if __name__ == "__main__" :
+#     i = MyIterator([1,2,3])
+#     for item in i :
+#         print(item)
+
+
+
+# class ReverseItertor :
+#     def __init__(self,data):
+#         self.data = data
+#         self.position = len(self.data) -1
+        
+#     def __iter__(self):
+#         return self
+    
+#     def __next__(self):
+#         if self.position <0 :
+#             raise StopIteration
+#         result = self.data[self.position]
+#         self.position -=1
+#         return result
+    
+
+
+# if __name__ == "__main__":
+#     i = ReverseItertor([1,2,3])
+#     for item in i:
+#         print(item)
+
+
+# def mygen():
+#     for i in range(1,1000):
+#         result = i * i
+#         yield result
+
+
+# gen = mygen()
+
+
+# gen = (i*i for i in range(1,1000))
+
+# print(next(gen))
+# print(next(gen))
+# print(next(gen))
+
+# class MyIterator:
+#     def __init__(self):
+#         self.data = 1
+
+#     def __iter__(self):
+#         return self
+    
+#     def __next__(self):
+#         result = self.data*self.data
+#         self.data += 1
+#         if self.data >= 1000:
+#             raise StopIteration
+#         return result
+
+
+# import time
+
+# def longtime_job():
+#     print("job start")
+#     time.sleep(1)
+#     return "done"
+
+# list_job = (longtime_job() for i in range(5))
+# # print(list_job[0])
+# print(next(list_job))
+
+# num : int = 1
+
+# def add(a:int, b:int) ->int:
+#     return a +b
+
+
+# result = add(3,3.4)
+# print(result)
+
+import re
+
+data = """
+park 800905-1049118
+kim 700905-1059119
+"""
+
+pat = re.compile("(\d{6})[-]\d{7}")
+print(pat.sub("\g<1>-*******", data))
