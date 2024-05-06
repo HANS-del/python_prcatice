f4b9d055e74c65f5d3e6df5a755fb57522c43677
HANSU <hahns38@gmail.com>
2024-05-06 14:13:12 +0900

    Study_기초_fianl

--- practice.py
+++ practice.py
@@ -1089,6 +1089,12 @@ def add(a,b):
 
 import time
 
+import csv
+
+import random
+
+import winsound
+
 name = input("what is your name?")
 
 print("Hi, " + name, "Time to play hangman game!")
@@ -1102,9 +1108,21 @@ print()
 time.sleep(0.5)
 
 
+words = []
+
+with open('./resource/word_list.csv', 'r') as f:
+    reader = csv.reader(f)
+    next(reader)
+    for c in reader:
+        words.append(c)
+
+
+random.shuffle(words)
+
+q = random.choice(words)
 
 # 정답 단어
-word = "secret"
+word = q[0].strip()
 
 
 #추측 단어 
@@ -1124,38 +1142,39 @@ while turns > 0:
         # 정답 단어 내에 추측 문자가 포함되어 있는 경우
         if char in guesses:
             #추측 단어 출력
-            print(char, end = '')
+            print(char, end = ' ')
         else:
             # 틀린 경우 대시로 처리
-            print("_", end='')
+            print("_", end=' ')
             failed +=1
-        if failed == 0:
-            print()
-            print()
-            print('Congratulation! The Guesses is correct.')
-            #while 구문 중단
-            break
 
+    #단어 추측이 성공한 경우    
+    if failed == 0:
         print()
-
-
-        #추측 단어 문자 단위 입력
         print()
-        guess = input("guess a charater.")
+        print('Congratulation! The Guesses is correct.')
+        #while 구문 중단
+        break
 
-        # 단어 더하기
-        guesses +=guess
+    print()
 
-        #정답 단어에 추측한 문자가 포함되어 있지 않으면
-        if guess not in word:
-            turns -=1
-            # 오류 메세지
-            print("Oops! Wrong")
-            # 남은 기회 출력
-            print("You have", turns, 'more guesses!')
-            
+    #추측 단어 문자 단위 입력
+    print()
+    guess = input("guess a charater.")
 
-            if turns == 0:
-                # 실패 메시지
-                print("You hangman game failed")
+    # 단어 더하기
+    guesses +=guess
+
+    #정답 단어에 추측한 문자가 포함되어 있지 않으면
+    if guess not in word:
+        turns -=1
+        # 오류 메세지
+        print("Oops! Wrong")
+        # 남은 기회 출력
+        print("You have", turns, 'more guesses!')
+        
+
+        if turns == 0:
+            # 실패 메시지
+            print("You hangman game failed")
 
